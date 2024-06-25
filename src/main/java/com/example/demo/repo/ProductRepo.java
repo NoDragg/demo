a8dc18480051858446eca1a492917a08c4d42b5d
Phi Long <quanphilong2003@gmail.com>
2024-06-25 16:52:38 +0700

    Changing sth

--- src/main/java/com/example/demo/repo/ProductRepo.java
+++ src/main/java/com/example/demo/repo/ProductRepo.java
@@ -2,7 +2,6 @@ package com.example.demo.repo;
 
 import com.example.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
